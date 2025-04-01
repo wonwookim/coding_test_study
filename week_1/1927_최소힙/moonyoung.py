7e3e36afaa93b9bcf6292850b8b706bdd7f11674
unknown <ansdud0135@gmail.com>
2025-04-01 20:49:23 +0900

    week_1업로드

---
+++ week_1/1927_최소힙/moonyoung.py
@@ -0,0 +1,16 @@
+import heapq
+import sys
+
+N = int(sys.stdin.readline().strip())
+
+hq = []
+
+for _ in range(N) :
+  X = int(sys.stdin.readline().strip())
+  if X > 0 :
+    heapq.heappush(hq, X)
+  elif X == 0 :
+    if len(hq) < 1 :
+      print(0)
+    else :
+      print(heapq.heappop(hq))

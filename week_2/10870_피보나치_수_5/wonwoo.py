479c20abcc4ac733bd32a292876c5668c3907f1b
wonwookim <kwo9911@naver.com>
2025-04-02 12:03:46 +0900

    Refactor: 재귀함수 코드 리펙토링

--- week_2/10870_피보나치_수_5/wonwoo.py
+++ week_2/10870_피보나치_수_5/wonwoo.py
@@ -1,4 +1,5 @@
 # 시간: 36ms, 메모리: 32412KB
+# 시간: 36ms, 메모리: 32412KB 동일
 # [0, 1] 기본 값
 # fn = fn-2 + fn-1
 # f1 = 0 + 1
@@ -10,11 +11,20 @@ input = sys.stdin.readline
 fibonacci_list = []
 num = int(input())
 
-def fibonacci_numbers(num, i):
-    if num < i:
-        return fibonacci_list[-1]
-    fibonacci_list.append(i if i < 2 else fibonacci_list[i-2] + fibonacci_list[i-1])
-    i += 1
-    return fibonacci_numbers(num, i)
+# def fibonacci_numbers(num, i):
+#     if num < i:
+#         return fibonacci_list[-1]
+#     fibonacci_list.append(i if i < 2 else fibonacci_list[i-2] + fibonacci_list[i-1])
+#     i += 1
+#     return fibonacci_numbers(num, i)
 
-print(fibonacci_numbers(num, 0))
+# print(fibonacci_numbers(num, 0))
+
+def fibonacci_numbers(num):
+    if num == 0:
+        return 0
+    if num == 1:
+        return 1
+    return fibonacci_numbers(num-1) + fibonacci_numbers(num-2)
+
+print(fibonacci_numbers(num))

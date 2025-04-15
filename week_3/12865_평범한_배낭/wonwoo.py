efed36f7d26650b6084e8c467517da68aa1faca2
wonwookim <kwo9911@naver.com>
2025-04-15 17:11:57 +0900

    add commit

--- week_3/12865_평범한_배낭/wonwoo.py
+++ week_3/12865_평범한_배낭/wonwoo.py
@@ -13,7 +13,7 @@ for i in range(1, N + 1):
     for j in range(1, k + 1): # 부피
         
         if j < w: # 지금 부피가 가능한 부피보다 더 큰 경우 
-            dp[i][j] = dp[i - 1][j]
+            dp[i][j] = dp[i - 1][j] # 바로 전 값을 가져오기기
         else:
-            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w] + weight_value[i - 1][1]) # 지금 선택 되었을 때, 나머지 부피에 대한 최대 값
+            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w] + weight_value[i - 1][1]) #  바로 전값, (지금 값을 넣었을 떄 나머지 부피에 대한 최대값 + 지금 값)의 최대 값값
 print(max(max(dp)))

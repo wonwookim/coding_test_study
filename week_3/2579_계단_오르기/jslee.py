dd851dc96150f6a2cbb833bd73ea1e6c03694d9d
wonwookim <kwo9911@naver.com>
2025-04-15 19:03:18 +0900

    code refactoring

--- week_3/2579_계단_오르기/jslee.py
+++ week_3/2579_계단_오르기/jslee.py
@@ -1,35 +1,23 @@
-Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> import sys
-... input = sys.stdin.readlines
-... 
-... # 입력 처리
-... lines = input()
-... n = int(lines[0].strip())
-... score = [int(x.strip()) for x in lines[1:]]
-... 
-... 
-... '''
-... score = []
-... 
-... for x in lines[1:]:
-...     x = x.strip()    
-...     num = int(x)      
-...     score.append(num)  
-... '''
-... 
-... # DP 배열
-... dp = [0] * n
-... 
-... # 초기값
-... dp[0] = score[0]
-... if n >= 2:
-...     dp[1] = score[0] + score[1]
-... if n >= 3:
-...     dp[2] = max(score[0] + score[2], score[1] + score[2])
-... 
-... # 점화식
-... for i in range(3, n):
-...     dp[i] = max(dp[i-2] + score[i], dp[i-3] + score[i-1] + score[i])
-... 
-... 
+import sys
+input = sys.stdin.readlines
+
+# 입력 처리
+lines = input()
+n = int(lines[0].strip())
+score = [int(x.strip()) for x in lines[1:]]
+
+# DP 배열
+dp = [0] * n
+
+# 초기값
+dp[0] = score[0]
+if n >= 2:
+    dp[1] = score[0] + score[1]
+if n >= 3:
+    dp[2] = max(score[0] + score[2], score[1] + score[2])
+
+# 점화식
+for i in range(3, n):
+    dp[i] = max(dp[i - 2] + score[i], dp[i - 3] + score[i - 1] + score[i])
+
+print(dp[-1])

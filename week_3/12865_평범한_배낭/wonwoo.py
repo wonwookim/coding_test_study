944d7457d54c373cc0d2f6e4035c41439d28af6d
wonwookim <kwo9911@naver.com>
2025-04-15 16:50:53 +0900

    wonwoo 12865_평범한_배낭 upload

--- week_3/12865_평범한_배낭/wonwoo.py
+++ week_3/12865_평범한_배낭/wonwoo.py
@@ -1,3 +1,4 @@
+# 시간: 4168ms 메모리: 229352KB
 import sys
 
 input = sys.stdin.readlines()
@@ -5,23 +6,14 @@ input = sys.stdin.readlines()
 N, k = map(int,input[0].strip().split())
 weight_value = [list(map(int, line.split())) for line in input[1:]]
 
-w_dp = [0] * (k + 1)  # 무게 누적용 리스트
-v_dp = [0] * (N + 1)  # 가치 저장용 리스트
+dp = [[0] * (k + 1)for i in range(N + 1)]
 
-for i in range(1, len(weight_value) + 1): # 상향식
-    print(i)
-    prev = k - w_dp[i]  # 이전은 전체 부피에서 현재 부피를 뺀 값이 최대임임
-    conv1 = 0
-    conv2 = 0
-    if 0 < k: # 누적 부피가 최대 부피보다 작을 떄
-        # 이전 값을 뺴고 
-        conv_1= v_dp[prev] + weight_value[i - 1][1]
-        # 이전 부피 + 현재 부피 더했을 때 최대 값
-        w_dp[i] = w_dp[i - 1] + weight_value[i - 1][0]
-    else:
-        w_dp[i] = w_dp[i - 1]
-        conv_2 = v_dp[i - 1]
-    v_dp[i] = max(conv_1, conv_2)
-
-print(w_dp)
-print(v_dp)
+for i in range(1, N + 1):
+    w, v = weight_value[i - 1]
+    for j in range(1, k + 1): # 부피
+        
+        if j < w: # 지금 부피가 가능한 부피보다 더 큰 경우 
+            dp[i][j] = dp[i - 1][j]
+        else:
+            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w] + weight_value[i - 1][1]) # 지금 선택 되었을 때, 나머지 부피에 대한 최대 값
+print(max(max(dp)))

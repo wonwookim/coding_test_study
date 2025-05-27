fd2ba7e9e08cc3dea26bbbf6e53a03b33d1a550f
노명구 <romanho23@naver.com>
2025-05-27 18:04:15 +0900

    Add files via upload

---
+++ week_9/1003_피보나치_함수/nmmm9.py
@@ -0,0 +1,14 @@
+# 32412kb	36ms
+
+t = int(input())
+dp = [[0, 0] for _ in range(41)]
+dp[0] = [1, 0]  # 0 호출 시: 0 = 1, 1 = 0
+dp[1] = [0, 1]  # 1 호출 시: 0 = 0, 1 = 1
+
+for i in range(2, 41):
+    dp[i][0] = dp[i-1][0] + dp[i-2][0]
+    dp[i][1] = dp[i-1][1] + dp[i-2][1]
+
+for _ in range(t):
+    n = int(input())
+    print(dp[n][0], dp[n][1])
